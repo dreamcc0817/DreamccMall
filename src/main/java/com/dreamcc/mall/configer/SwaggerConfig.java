cb320efcc342461d2acdd08d37da5a74232256bc
dreamcc <dreamcc0817@gmail.com>
2018-08-11 11:06:16 +0800

    Initial commit 2018-8-11

---
+++ src/main/java/com/dreamcc/mall/configer/SwaggerConfig.java
@@ -0,0 +1,74 @@
+package com.dreamcc.mall.configer;
+
+import com.google.common.base.Predicate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static com.google.common.base.Predicates.or;
+import static springfox.documentation.builders.PathSelectors.regex;
+
+/**
+ * @Title: DreamccMall
+ * @Package: com.dreamcc.mall.configer
+ * @Description: springboot swagger config
+ * @Author: dreamcc
+ * @Date: 2018/8/11 9:16
+ * @Version: V1.0
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+	@Value("${server.servlet-path}")
+	private String pathMapping;
+
+	private ApiInfo initApiInfo() {
+		ApiInfo apiInfo = new ApiInfo("DreamccMall Project Platform API",//Title
+				initContextInfo(),//Simple Description
+				"1.0.0",//Version
+				"",
+				"team by dreamcc",//Editor
+				"The Apache License, Version 2.0",//Link Display Font
+				"http://www.dreamcc.com"//Web Links
+		);
+
+		return apiInfo;
+	}
+
+	private String initContextInfo() {
+		StringBuffer sb = new StringBuffer();
+
+		return sb.toString();
+	}
+
+	public Docket restfulApi(){
+		System.out.println("http://localhost:8080/" + pathMapping + "/swagger-ui.html");
+		return new Docket(DocumentationType.SWAGGER_2)
+				.groupName("dreamcc")
+				.genericModelSubstitutes(ResponseEntity.class)
+				.useDefaultResponseMessages(true)
+				.forCodeGeneration(false)
+				.pathMapping(pathMapping)
+				.select()
+				.paths(doFilteringRules())
+				.build()
+				.apiInfo(initApiInfo());
+	}
+
+	/**
+	 * set filter regular
+	 * filter support regex
+	 * @return
+	 */
+	private Predicate<String> doFilteringRules() {
+		return or(
+				regex("/hello.*"),
+				regex("/vehicles.*")
+		);
+	}
+}
---
+++ src/main/java/com/dreamcc/mall/service/packageinfo.java
@@ -0,0 +1,12 @@
+package com.dreamcc.mall.service;
+
+/**
+ * @Title: DreamccMall
+ * @Package: com.dreamcc.mall.service
+ * @Description:功能类
+ * @Author: dreamcc
+ * @Date: 2018/8/10 21:36
+ * @Version: V1.0
+ */
+public class packageinfo {
+}
